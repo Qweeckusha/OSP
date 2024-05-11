b9c3e78d0ff473c6a4b3297c7f1e22a382e74810
ggume <ggumennik@gmail.com>
2024-05-12 01:34:17 +1000

    placeholder text added

--- main_admin/forms.py
+++ main_admin/forms.py
@@ -1,13 +1,13 @@
 from django import forms
 
 class LoginForm(forms.Form):
-    queue_id = forms.CharField(label='Номер в очереди', max_length=10)
+    queue_id = forms.CharField(label='Номер в очереди', max_length=10, widget=forms.TextInput(attrs={'placeholder': 'Ваш номер'}))
 
 class LoginFormAdmin(forms.Form):
-    username = forms.CharField(label='Логин', max_length=40)
-    password = forms.CharField(label='Пароль', max_length=100)
+    username = forms.CharField(label='Логин', max_length=40, widget=forms.TextInput(attrs={'placeholder': 'Логин'}))
+    password = forms.CharField(label='Пароль', max_length=100, widget=forms.TextInput(attrs={'placeholder': 'Пароль'}))
 
 class RegFormAdmin(forms.Form):
-    username = forms.CharField(label='Логин', max_length=40)
-    password = forms.CharField(label='Пароль', max_length=100)
-    key = forms.CharField(label='Ключ', max_length=12)
+    username = forms.CharField(label='Логин', max_length=40, widget=forms.TextInput(attrs={'placeholder': 'Логин'}))
+    password = forms.CharField(label='Пароль', max_length=100, widget=forms.TextInput(attrs={'placeholder': 'Пароль'}))
+    key = forms.CharField(label='Ключ', max_length=12, widget=forms.TextInput(attrs={'placeholder': 'Ключ '}))
