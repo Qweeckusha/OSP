dbac0382dddd27e094576e000e1f1292dafbaa90
Qweeckusha <tolya.zaytsev.2004@gmail.com>
2024-05-15 02:18:00 +1100

    migrations

--- main_admin/forms.py
+++ main_admin/forms.py
@@ -8,6 +8,6 @@ class LoginFormAdmin(forms.Form):
     password = forms.CharField(label='Пароль', max_length=100, widget=forms.TextInput(attrs={'placeholder': 'Пароль'}))
 
 class RegFormAdmin(forms.Form):
-    username = forms.CharField(label='Логин', max_length=40, widget=forms.TextInput(attrs={'placeholder': 'Логин'}))
-    password = forms.CharField(label='Пароль', max_length=100, widget=forms.TextInput(attrs={'placeholder': 'Пароль'}))
-    key = forms.CharField(label='Ключ', max_length=12, widget=forms.TextInput(attrs={'placeholder': 'Ключ '}))
+    username = forms.CharField(max_length=40, widget=forms.TextInput(attrs={'placeholder': 'Логин'}))
+    password = forms.CharField(max_length=100, widget=forms.TextInput(attrs={'placeholder': 'Пароль'}))
+    key = forms.CharField(max_length=12, widget=forms.TextInput(attrs={'placeholder': 'Ключ '}))
