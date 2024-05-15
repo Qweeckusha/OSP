d570df3b11cc6497fb31c25cd762e117bd18e467
Qweeckusha <tolya.zaytsev.2004@gmail.com>
2024-05-16 00:54:41 +1100

    Some fixes

--- main/urls.py
+++ main/urls.py
@@ -5,8 +5,6 @@ from main_admin.views import main_admin_page, login_users, login_admins
 
 urlpatterns = [
     path('', main_user_page, name='main'),
-    path('admin_page/', main_admin_page, name='admin_page'),
     path('login/', login_users, name='login'),
     path('login_admin/', login_admins, name='login_admins'),
-
 ]
